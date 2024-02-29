fb30323ca7067a13086b3d8ba75ae3edc97610c3
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2024-02-29 16:09:00 -0300

    Update VSCode and Django settings

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,13 @@
-# from django.shortcuts import render
-from typing import Any
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def home(request) -> HttpResponse:
-    '''define a home'''
-    return HttpResponse('home')
+def home(request):
+    return render(request, 'recipes/home.html')
 
 
-def contato(request: Any) -> HttpResponse:
-    return HttpResponse('contato')
+def contato(request):
+    return render(request, 'recipes/contato.html')
 
 
-def sobre(request: Any) -> HttpResponse:
-    return HttpResponse('sobre')
+def sobre(request):
+    return render(request, 'recipes/sobre.html')
