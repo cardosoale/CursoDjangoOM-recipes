bd4b5bcd9689ee8ac5b17a1de4eb0696fae44246
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2024-03-01 16:17:32 -0300

    Delete unnecessary HTML files , update home view, add templates

--- recipes/views.py
+++ recipes/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Alexandre',
+    })
