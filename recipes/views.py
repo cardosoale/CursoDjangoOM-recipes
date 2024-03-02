ce4f1cce1648be952b238abed01b201a8283c456
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2024-03-02 11:44:09 -0300

    Refactor URLs and add recipes view

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Alexandre',
     })
+
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Alexandre',
+    })
