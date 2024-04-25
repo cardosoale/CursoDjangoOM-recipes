30994a4819e0bd7b2b00cb3a555c2363d2f4792e
Alexandre Cardoso <cardoal01@gmail.com>
2024-04-25 12:49:50 -0300

    aula 111

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.db.models import Q
 
 from recipes.models import Recipe
 
@@ -38,10 +39,21 @@ def recipe(request, id):
 
 
 def search(request):
-    search_term = request.GET.get('q')
+    search_term = request.GET.get('q', '').strip()
 
     if not search_term:
         raise Http404()
 
-    return render(request, 'recipes/pages/search.html',
-                  {'page_title': f'Search for "{search_term}" |'},)
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
+    })
