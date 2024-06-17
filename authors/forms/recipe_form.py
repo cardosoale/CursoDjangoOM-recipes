fa30294086f727d5dc031ebf386181f57b7a5aec
Alexandre Cardoso <cardoal01@gmail.com>
2024-06-17 13:09:18 -0300

    aula 188

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,17 +1,36 @@
-from django.forms import ModelForm
+from django import forms
 from recipes.models import Recipe
 from utils.django_forms import add_attr
 
 
-class AuthorRecipeForm(ModelForm):
+class AuthorRecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
-        add_attr(self.fields.get('cover'), 'class', 'span-2')
 
     class Meta:
         model = Recipe
         fields = ('title', 'description', 'preparation_time',
                   'preparation_time_unit', 'servings', 'servings_unit',
                   'preparation_steps', 'cover')
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Portions', 'Portions'),
+                    ('Pieces', 'Pieces'),
+                    ('Peoples', 'Peoples'),
+                ),
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutes', 'Minutes'),
+                    ('Hours', 'Hours')
+                )
+            )
+        }
