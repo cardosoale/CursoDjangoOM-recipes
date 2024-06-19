a302aa206c2b2dd0f930baa4c9b314c6855b5f7f
Alexandre Cardoso <cardoal01@gmail.com>
2024-06-19 15:31:54 -0300

    aula 190

--- authors/views.py
+++ authors/views.py
@@ -142,3 +142,31 @@ def dashboard_recipe_edit(request, id):
             'form': form
         }
     )
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_new(request):
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Your recipe has been successfully created!')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
+
+    return render(
+        request,
+        'authors/pages/dashboard_recipe.html',
+        context={
+            'form': form
+        }
+    )
