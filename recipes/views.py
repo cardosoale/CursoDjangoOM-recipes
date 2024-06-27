a42f2af8536bd65c7ad346ec8c6443dd5c43c685
Alexandre Cardoso <cardoal01@gmail.com>
2024-06-27 19:17:24 -0300

    aula 207

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView
 from django.db.models import Q
-from django.http.response import Http404
+from django.http.response import Http404, HttpResponseRedirect
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.pagination import make_pagination
+from django.urls import reverse
 
 from recipes.models import Recipe
 
@@ -88,7 +89,7 @@ class RecipeListViewBase(ListView):
         return qs
 
     def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super().get_context_data(*args, **kwargs)
         page_obj, pagination_range = make_pagination(
             self.request,
             context.get('recipes'),
@@ -102,5 +103,63 @@ class RecipeListViewBase(ListView):
         return context
 
 
-class RecipeListViewBaseHome(RecipeListViewBase):
+class RecipeListViewHome(RecipeListViewBase):
     template_name = 'recipes/pages/home.html'
+
+
+class RecipeListViewCategory(RecipeListViewBase):
+    template_name = 'recipes/pages/category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = get_list_or_404(qs.filter(
+            category__id=self.kwargs.get('category_id')),
+        )
+
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context['title'] = f"{context['recipes'][0].category.name} - " \
+            "Category | "
+
+        return context
+
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
+
+    def get(self, request, *args, **kwargs):
+        search_term = request.GET.get('q', '').strip()
+        if not search_term:
+            raise Http404("No search term provided.")
+
+        if request.GET.get('q') != search_term:
+            clean_url = f"{reverse('recipes:search')}?q={search_term}"
+            return HttpResponseRedirect(clean_url)
+
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '').strip()
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            Q(
+                Q(title__icontains=search_term) |
+                Q(description__icontains=search_term),
+            )
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '').strip()
+
+        context.update({
+            'page_title': f'Search for "{search_term}" |',
+            'search_term': search_term,
+            'additional_url_query': f'&q={search_term}',
+        })
+
+        return context
